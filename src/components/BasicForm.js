1341714a33180a7676a085ef74a7840afc07008f
vatehav <viliam.vateha@uzis.cz>
2022-09-10 21:20:11 +0200

    [FEATURE] add input hook to all input fields

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -3,6 +3,49 @@ import useInput from "../hooks/useInput";
 const isNotEmpty = (value) => value.trim() !== "";
 
 const BasicForm = () => {
+  // First name
+  const {
+    value: firstNameInput,
+    valueIsValid: firstNameInputIsValid,
+    valueHasError: firstNameInputIsInvalid,
+    inputValueChangeHandler: firstNameChangeHandler,
+    inputValueBlurHandler: firstNameBlurHandler,
+    resetInput: firstNameResetInput,
+  } = useInput(isNotEmpty);
+
+  // Last name
+  const {
+    value: lastNameInput,
+    valueIsValid: lastNameInputIsValid,
+    valueHasError: lastNameInputIsInvalid,
+    inputValueChangeHandler: lastNameChangeHandler,
+    inputValueBlurHandler: lastNameBlurHandler,
+    resetInput: lastNameResetInput,
+  } = useInput(isNotEmpty);
+
+  // Email
+  // TODO need to update the email validation function
+  const {
+    value: emailInput,
+    valueIsValid: emailInputIsValid,
+    valueHasError: emailInputIsInvalid,
+    inputValueChangeHandler: emailChangeHandler,
+    inputValueBlurHandler: emailBlurHandler,
+    resetInput: emailResetInput,
+  } = useInput(isNotEmpty);
+
+  // Phone
+  // TODO need to update the phone validation function
+  const {
+    value: phoneInput,
+    valueIsValid: phoneInputIsValid,
+    valueHasError: phoneInputIsInvalid,
+    inputValueChangeHandler: phoneChangeHandler,
+    inputValueBlurHandler: phoneBlurHandler,
+    resetInput: phoneResetInput,
+  } = useInput(isNotEmpty);
+
+  // Textarea
   const {
     value: textInput,
     valueIsValid: textInputIsValid,
@@ -14,7 +57,7 @@ const BasicForm = () => {
 
   let formIsValid = false;
 
-  if (textInputIsValid) {
+  if (emailInputIsValid && phoneInputIsValid && textInputIsValid) {
     formIsValid = true;
   }
 
@@ -25,7 +68,18 @@ const BasicForm = () => {
       return;
     }
 
-    console.log(textInput);
+    console.log(
+      "Form input values:",
+      firstNameInput,
+      lastNameInput,
+      emailInput,
+      phoneInput,
+      textInput
+    );
+    firstNameResetInput();
+    lastNameResetInput();
+    emailResetInput();
+    phoneResetInput();
     textResetInput();
   };
   return (
@@ -33,19 +87,47 @@ const BasicForm = () => {
       <form onSubmit={formSubmitHandler}>
         <div>
           <label htmlFor="first-name">First Name:</label>
-          <input type="text" id="first-name"></input>
+          <input
+            type="text"
+            id="first-name"
+            value={firstNameInput}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          ></input>
+          {firstNameInputIsInvalid && <p>First name is invalid</p>}
         </div>
         <div>
           <label htmlFor="last-name">Last Name:</label>
-          <input type="text" id="last-name"></input>
+          <input
+            type="text"
+            id="last-name"
+            value={lastNameInput}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          ></input>
+          {lastNameInputIsInvalid && <p>Last name is invalid</p>}
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email"></input>
+          <input
+            type="email"
+            id="email"
+            value={emailInput}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+          ></input>
+          {emailInputIsInvalid && <p>Email is invalid</p>}
         </div>
         <div>
           <label htmlFor="phone">Phone:</label>
-          <input type="tel" id="phone"></input>
+          <input
+            type="tel"
+            id="phone"
+            value={phoneInput}
+            onChange={phoneChangeHandler}
+            onBlur={phoneBlurHandler}
+          ></input>
+          {phoneInputIsInvalid && <p>Phone is invalid</p>}
         </div>
         <div>
           <label htmlFor="text">Last Name:</label>
@@ -57,7 +139,7 @@ const BasicForm = () => {
             onChange={textChangeHandler}
             onBlur={textBlurHandler}
           ></textarea>
-          {textInputIsInvalid && <p>Text is empty or invalid</p>}
+          {textInputIsInvalid && <p>Text is invalid</p>}
         </div>
         <div>
           <button disabled={!formIsValid}>Submit</button>
